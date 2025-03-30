3f3eea12fd9ebd62d14e3dd002f7f3e47659d392
Wesley Goarant <Wesleydu29860@gmail.com>
2025-03-30 19:27:32 +0200

    ajout d'une fonction de sauvegarde dans le localstorage, fin du projet

--- Toggle-button/main.js
+++ Toggle-button/main.js
@@ -4,9 +4,33 @@ const btn = document.querySelector(".btn");
 
 const icon = document.querySelector("i");
 
+// function afin de sauvegarder la preference de l'utilisateur dans le local storage
+function savePreference(value){
+    localStorage.setItem("dark-mode", value);
+}
+
+// function pour recuperer la preference de l'utilisateur
+function getPreference(){
+    const darkmode = localStorage.getItem("dark-mode");
+
+    if (!darkmode) {
+        savePreference(false);
+        icon.classList.add("fa-sun");
+    } else if(darkmode == "true") {
+        body.classList.add("dark-mode");
+        icon.classList.add("fa-moon");
+    }else if(darkmode == "false") {
+        icon.classList.add("fa-sun");
+    }
+}
+getPreference();
+
 btn.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
     icon.classList.add("animated");
+
+    // si le mode sombre est activé, on sauvegarde la preference de l'utilisateur
+    savePreference(body.classList.contains("dark-mode"));
 
     if (body.classList.contains("dark-mode")) {
         icon.classList.remove("fa-sun");
